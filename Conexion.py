387b410d67f521b38e5adc991590c649482ab9b5
m0nT3cR1s70 <mizar0.30@gmail.com>
2022-11-10 18:51:00 -0600

    Genera la clase para la conexion

--- Conexion.py
+++ Conexion.py
@@ -1,21 +1,51 @@
-import psycopg2
+import psycopg2 as bd
+from logger_base import log
+import sys
 
 class Conexion:
-	DATABASE = 'Test'
-	HOST     = 'localhost'
-	PASSWORD = 'administror'
-	PORT     = 5432
-	USER     = 'm0nT3cR1s70'
+	_DATABASE = 'Test'
+	_HOST     = 'localhost'
+	_PASSWORD = 'administror'
+	_PORT     = 5432
+	_USER     = 'm0nT3cR1s70'
+	_conexion = None
+	_cursor   = None
+
+	@classmethod
+	def obtenerConexion(cls):
+		if cls._conexion is None:
+			try:
+				cls._conexion = bd.connect(
+					database  = cls._DATABASE,
+					host      = cls._HOST,
+					password  = cls._PASSWORD, 
+					port      = cls._PORT,
+					user      = cls._USER
+				)
+				log.debug(f'Conexion exitosa {cls._conexion}')
+				return cls._conexion
+			except Exception as e:
+				log.error(f'Ocurrion una exception {e} conexion') 
+				sys.exit()
+		else:
+			return cls._conexion
+
+	@classmethod
+	def obtenerCursor(cls):
+		if cls._cursor is None:
+			try:
+				cls._cursor = cls.obtenerConexion().cursor()
+				log.debug(f'Cursor abierto {cls._cursor}')
+			except Exception as e:
+				log.error(f'Ocurrion una exception {e} cursor')
+				sys.exit()
+		else:
+			return cls._cursor
+
+
+
 
 if __name__ == '__main__':
-
-	DATABASE = 'Test'
-	HOST     = 'localhost'
-	PASSWORD = 'administror'
-	PORT     = 5432
-	USER     = 'm0nT3cR1s70'
-	conexion = psycopg2.connect(database=DATABASE,
-		host=HOST,
-		password=PASSWORD,
-		port=PORT,
-		user=USER)
+	conexion = Conexion()
+	#conexion.obtenerconexion()
+	conexion.obtenerCursor()
